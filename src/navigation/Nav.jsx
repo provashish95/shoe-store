9817811d352da40c83cc82184963e04dc160e363
provashish95 <roytonmoy95@gmail.com>
2023-07-13 11:52:01 +0600

    updated some code

--- src/navigation/Nav.jsx
+++ src/navigation/Nav.jsx
@@ -1,8 +1,28 @@
 import './Nav.css'
+import { FiHeart } from 'react-icons/fi'
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from 'react-icons/ai'
 
 function Nav() {
-  return (
-    <div>Nav</div>
-  )
+  return <nav>
+    <div className="nav-container">
+      <input type="text" className='search-input' placeholder="Search your shoe" />
+    </div>
+
+    <div className="profile-container">
+      <a href="#">
+        <FiHeart className='nav-icons' />
+      </a>
+
+      <a href="#">
+        <AiOutlineShoppingCart className='nav-icons' />
+      </a>
+
+      <a href="#">
+        <AiOutlineUserAdd className='nav-icons' />
+      </a>
+    </div>
+
+
+  </nav>
 }
 export default Nav;
